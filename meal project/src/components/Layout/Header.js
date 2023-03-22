b29293f4875a721ae5831eeaded78cec96dd5515
Bmattholder <bmattholder@gmail.com>
2023-03-22 16:49:55 -0500

    React meals complete

--- meal project/src/components/Layout/Header.js
+++ meal project/src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.png";
 import classes from "./Header.module.css";
 function Header(props) {
@@ -7,7 +8,7 @@ function Header(props) {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="table of food" />
