3b65f0da091d770d4448e4e12ea774bc318540d8
Bmattholder <bmattholder@gmail.com>
2023-04-02 17:37:23 -0500

    React-router start

---
+++ 12-adv-starting-project/frontend/src/pages/EditEvent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function EditEvent(props) {
+    return (
+        <h1>EditEventPage</h1>
+    );
+}
+
+export default EditEvent;
---
+++ 12-adv-starting-project/frontend/src/pages/NewEvent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewEvent(props) {
+  return <h1>NewEventPage</h1>;
+}
+
+export default NewEvent;
